4b1d440a58bcef2274e28bff8ad944ce0e482916
nathaliabartholo2808 <nathaliabartholo1996@gmail.com>
2021-06-01 17:32:46 -0300

    3 commit

---
+++ js/login.js
@@ -0,0 +1,8 @@
+function getUserInfo(){
+    var user = document.getElementById("user").value
+    var password = document.getElementById("password").value
+
+
+    window.location.replace("///C:/Users/tatin/Google%20Drive/Est%C3%A1gio/Repositorios/SelecaoDirectionSystems/html/Menus.html")
+
+}
