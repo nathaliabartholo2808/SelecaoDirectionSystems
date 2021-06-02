bba2167488117b5decd7c65a4f2fe98898074aaf
nathaliabartholo2808 <nathaliabartholo1996@gmail.com>
2021-06-02 11:27:13 -0300

    4 commit

---
+++ TelaMenus/Menu.js
@@ -0,0 +1,11 @@
+function mudarTela(tela){
+    switch(tela){
+        case "cadastrar":
+            window.location.replace("")
+        case "consultar" :
+            window.location.replace("")
+        case "deslogar":
+            window.location.replace("C:/Users/tatin/Google%20Drive/Est%C3%A1gio/Repositorios/SelecaoDirectionSystems/TelaLogin/index.html")
+    }
+
+}
--- js/login.js
+++
@@ -1,8 +0,0 @@
-function getUserInfo(){
-    var user = document.getElementById("user").value
-    var password = document.getElementById("password").value
-
-
-    window.location.replace("///C:/Users/tatin/Google%20Drive/Est%C3%A1gio/Repositorios/SelecaoDirectionSystems/html/Menus.html")
-
-}
